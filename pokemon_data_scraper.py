2ba84281c02b72a84b7d94ab057ca4cc4618ac60
Olivia <olivia.markow@mail.utoronto.ca>
2025-02-27 15:55:40 -0500

    updated pokemon_data_scraper.py

--- pokemon_data_scraper.py
+++ pokemon_data_scraper.py
@@ -1,29 +1,34 @@
 import csv
 
-def get_pokemon_data(pokemon_id: int, filename: str) -> list:
-    """Return the data for a specific Pokemon
-    
-    Preconditions:
-      - pokemon_id is a valid Pokemon id
-    """
-    with open(filename) as file:
-        reader = csv.reader(file)
-        next(reader)  # skip header row
-        for row in reader:
-            if int(row[0]) == pokemon_id:
-              return process_row(row)
+def get_pokemon_data(pokemon_ids: tuple[int], filename: str) -> list:
+  """Return the data for a specific Pokemon
+  
+  Preconditions:
+    - pokemon_ids are a valid Pokemon ids
+  """
+  data = []
+
+  with open(filename) as file:
+      reader = csv.reader(file)
+      next(reader)  # skip header row
+
+      for row in reader:
+        if int(row[0]) in pokemon_ids:
+          data.append(process_row(row))
+          if len(data) == len(pokemon_ids):
+              return data
 
 def process_row(row: list[str]) -> list:
-    """Convert a row of pokemon data to a list with more appropriate data types."""
-    return [int(row[0]), 
-            row[1], 
-            row[2], 
-            row[3], 
-            int(row[4]), 
-            int(row[5]), 
-            int(row[6]), 
-            int(row[7]), 
-            int(row[8]), 
-            int(row[9])]
+  """Convert a row of pokemon data to a list with more appropriate data types."""
+  return [int(row[0]), 
+          row[1], 
+          row[2], 
+          row[3], 
+          int(row[4]), 
+          int(row[5]), 
+          int(row[6]), 
+          int(row[7]), 
+          int(row[8]), 
+          int(row[9])]
 
-print(get_pokemon_data(1, "pokemon_data.csv"))
+print(get_pokemon_data((1, 2, 3), "pokemon_data.csv"))
