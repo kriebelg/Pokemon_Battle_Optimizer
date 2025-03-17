f9463b341233e8f6eca68804e667a6b9b6b6023e
Yash Jain <77806168+InfiniteInbox@users.noreply.github.com>
2025-03-16 23:47:25 -0400

    finished score

--- graph_algorithm.py
+++ graph_algorithm.py
@@ -13,6 +13,7 @@ def graph_builder(file_path):
             graph.add_attacking_edge(po_type, pok_type, effectiveness[type_indices[po_type]][type_indices[pok_type]])
     return graph
 
+
 def strong_weak(chosen_pokemons):
     strong = {}
     weak = {}
@@ -50,6 +51,8 @@ def strong_weak(chosen_pokemons):
                     else:
                         strong[poke] = 1
     return strong, weak
+
+
 def dict_subtraction(strong, weak):
     final_dict = {}
     for poke_type in weak:
@@ -61,6 +64,43 @@ def dict_subtraction(strong, weak):
             final_dict[poke_type] = weak[poke_type]
 
     return final_dict
+
+def score_assigner(final_dict, enemy_types, graph):
+    temp = {}
+    for key in final_dict:
+        defense_0 = 0
+        defense_2 = 0
+        defense_5 = 0
+        attack_0 = 0
+        attack_2 = 0
+        attack_5 = 0
+        one_point_zero = 0
+        key = key.capitalize()
+        for type in enemy_types:
+            if type in graph.vertices[key].incoming_neighbors[0.0]:
+                defense_0 += 1
+            elif type in graph.vertices[key].incoming_neighbors[0.5]:
+                defense_5 += 1
+            elif type in graph.vertices[key].incoming_neighbors[2.0]:
+                defense_2 += 1
+            elif type in graph.vertices[key].incoming_neighbors[1.0]:
+                one_point_zero += 1
+            elif type in graph.vertices[key].outgoing_neighbors[0.0]:
+                attack_0 += 1
+            elif type in graph.vertices[key].outgoing_neighbors[0.5]:
+                attack_5 += 1
+            elif type in graph.vertices[key].outgoing_neighbors[2.0]:
+                attack_2 += 1
+            elif type in graph.vertices[key].incoming_neighbors[1.0]:
+                one_point_zero += 1
+        final_score = final_dict[key]*((30*defense_0)+(5*attack_2)+(3*defense_5)+(0.1*one_point_zero)-(4.9*defense_2)-(30*attack_0)-(2.9*attack_5))
+        temp[key] = final_score
+    return temp
+
+
+
+
+
 
 
 
